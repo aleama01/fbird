0d1381a1c2ea200a0c5e455b4292607c5665a041
aleama01 <aleama280301@gmail.com>
2024-03-18 19:13:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>fBird</h1>
+      <h3>fuck shit fuck shit</h3>
+    </div>
+  );
+}
+
+export default App;
