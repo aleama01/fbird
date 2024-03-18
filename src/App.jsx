acff6f49c382ecc8c732ad30b7089dc84a794f72
aleama01 <aleama280301@gmail.com>
2024-03-18 20:10:23 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Swr from './Swr';
+
 
 function App() {
+  const [img, setImg] = useState()
+
+
   return (
     <div className="App">
       <h1>fBird</h1>
       <h3>fuck shit fuck shit</h3>
+      <Swr />
     </div>
   );
 }
