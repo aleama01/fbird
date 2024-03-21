8648a301eac1bcc8d9b83fac34ae1ed6785f984c
aleama01 <aleama280301@gmail.com>
2024-03-21 18:42:47 +0200

    changed layout

--- src/pages/yourBirds.js
+++ src/pages/yourBirds.js
@@ -16,11 +16,11 @@ function YourBirds() {
   }, []);
 
   return (
-    <div className='flex flex-col justify-start items-start mx-10 h-[100dvh]'>
-      <div className='flex flex-row space-x-6 my-2'>
+    <div className='flex flex-col justify-start items-start mx-10 h-[100dvh] '>
+      <div className='flex flex-row justify-between mx-auto w-5/6 my-2 flex-wrap'>
         {photos.map((photo, index) => (
-          <div key={photo.url} className="w-[200px] h-[200px] hover:brightness-100 brightness-75">
-            <img src={photo.url} alt={photo.name} />
+          <div key={photo.url} className='basis-1/5 my-2'>
+            <img src={photo.url} className="w-[200px] object-cover h-[200px] hover:brightness-100 brightness-75" alt={photo.name} />
             <p>{photo.name}</p>
           </div>
         ))}
